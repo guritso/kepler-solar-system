1c33930b178d3127690a98a5c40e0b17b465925e
guritso <gurizenit@gmail.com>
2025-10-01 04:42:56 -0300

    refactor: improve Canvas component structure and event handling

--- src/lib/components/Canvas.svelte
+++ src/lib/components/Canvas.svelte
@@ -1,165 +1,153 @@
 <script lang="ts">
-	import { coordinates } from "$lib/stores";
+	import { coordinates } from '$lib/stores';
 
-    let canvas: HTMLCanvasElement;
-    let ctx: CanvasRenderingContext2D;
+	let canvas: HTMLCanvasElement;
+	let ctx: CanvasRenderingContext2D;
 
-    
-    let offsetX = $state(0);
-    let offsetY = $state(0);
-    let scale = $state(1);
-    let gridSize = $state(60);
-    let isDragging = false;
-    let startX: number, startY: number;
-    
-    let mouseX = $state(0);
-    let mouseY = $state(0);
-    
-    $effect(() => {
-        canvas.height = window.innerHeight;
-        canvas.width = window.innerWidth;
+	let offsetX = $state(0);
+	let offsetY = $state(0);
+	let scale = $state(1);
+	let gridSize = $state(20);
+	let isDragging = false;
+	let startX: number, startY: number;
 
-        ctx = canvas.getContext("2d")!;
+	let cursorX = $state(0);
+	let cursorY = $state(0);
+	let centerX = $state(0);
+	let centerY = $state(0);
 
-        console.table({
-        offsetX,
-            offsetY,
-            scale,
-            gridSize,
-            isDragging,
-            startX,
-            startY,
-            mouseX, 
-            mouseY,
-        });
-    
-        draw();
-    });
+	$effect(() => {
+		if (!canvas) return;
 
-    function draw() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+		ctx = canvas.getContext('2d')!;
 
-        ctx.save();
-        ctx.translate(canvas.width / 2 + offsetX, canvas.height / 2 + offsetY);
-        ctx.scale(scale, scale);
+		resize();
 
-        ctx.strokeStyle = "gray";
-        ctx.lineWidth = 1 / scale;
+		window.addEventListener('resize', resize);
 
-        // temporary grid
-        const canvasSize = 300000;
+		return () => {
+			window.removeEventListener('resize', resize);
+		};
+	});
 
-        for (let x = -canvasSize / 2; x < canvasSize / 2; x += gridSize) {
-            ctx.beginPath();
-            ctx.moveTo(x, -canvasSize / 2);
-            ctx.lineTo(x, canvasSize / 2);
-            ctx.stroke();
-        }
+	function draw() {
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-        for (let y = -canvasSize / 2; y < canvasSize / 2; y += gridSize) {
-            ctx.beginPath();
-            ctx.moveTo(-canvasSize / 2, y);
-            ctx.lineTo(canvasSize / 2, y);
-            ctx.stroke();
-        }
+		ctx.save();
+		ctx.translate(canvas.width / 2 + offsetX, canvas.height / 2 + offsetY);
+		ctx.scale(scale, scale);
 
-        // temporary sun
-        ctx.fillStyle = "yellow";
-        ctx.beginPath();
-        ctx.arc(0, 0, 5, 0, Math.PI * 2);
-        ctx.fill();
+		ctx.strokeStyle = 'gray';
+		ctx.lineWidth = 0.5 / scale;
 
-        ctx.restore();
-    }
+		// temporary grid
+		const canvasSize = 300000;
 
-    // pan events
-    function mousedown (event: MouseEvent) {
-        isDragging = true;
-        startX = event.clientX - offsetX;
-        startY = event.clientY - offsetY;   
-    }
+		for (let x = -canvasSize / 2; x < canvasSize / 2; x += gridSize) {
+			ctx.beginPath();
+			ctx.moveTo(x, -canvasSize / 2);
+			ctx.lineTo(x, canvasSize / 2);
+			ctx.stroke();
+		}
 
-    function mouseup () {
-        isDragging = false;
-    }
+		for (let y = -canvasSize / 2; y < canvasSize / 2; y += gridSize) {
+			ctx.beginPath();
+			ctx.moveTo(-canvasSize / 2, y);
+			ctx.lineTo(canvasSize / 2, y);
+			ctx.stroke();
+		}
 
-    function mouseleave () {
-        isDragging = false;
-    }
+		// temporary sun
+		ctx.fillStyle = 'yellow';
+		ctx.beginPath();
+		ctx.arc(0, 0, 5, 0, Math.PI * 2);
+		ctx.fill();
 
-    function mousemove (event: MouseEvent) {
-        mouseX = event.clientX;
-        mouseY = event.clientY;
+		ctx.restore();
+	}
 
-        if (isDragging) {
-            offsetX = event.clientX - startX;
-            offsetY = event.clientY - startY;
-        }
+	// pan events
+	function pointerdown(event: PointerEvent) {
+		event.preventDefault();
 
-        draw();
-    }
+		isDragging = true;
+		startX = event.clientX - offsetX;
+		startY = event.clientY - offsetY;
+	}
 
-    const zoomMax = 10;
-    const zoomMin = 0.1;
+	function pointerup() {
+		isDragging = false;
+	}
 
-    // zoom events
-    function wheel (event: WheelEvent) {
-        event.preventDefault();
+	function pointerleave() {
+		isDragging = false;
+	}
 
-        const zoomSpeed = 0.1;
-        const oldScale = scale;
+	const zoomMax = 10;
+	const zoomMin = 0.1;
 
-        scale *= event.deltaY > 0 ? (1 - zoomSpeed) : (1 + zoomSpeed);
-        scale = Math.max(zoomMin, Math.min(scale, zoomMax)); // limit zoom
-        
-        // keep zoom on the cursor
-        const cursorX = event.clientX - canvas.width / 2;
-        const cursorY = event.clientY - canvas.height / 2;
+	// zoom events
+	function wheel(event: WheelEvent) {
+		event.preventDefault();
 
-        offsetX = cursorX - (cursorX - offsetX) * (scale / oldScale);
-        offsetY = cursorY - (cursorY - offsetY) * (scale / oldScale);
-        
-        draw();
-    }
+		const zoomSpeed = 0.1;
+		const oldScale = scale;
 
-    // resize canvas
-    function resize () {
-        canvas.height = window.innerHeight;
-        canvas.width = window.innerWidth;
-        draw();
-    }
+		scale *= event.deltaY > 0 ? 1 - zoomSpeed : 1 + zoomSpeed;
+		scale = Math.max(zoomMin, Math.min(scale, zoomMax)); // limit zoom
 
-    let cursorX = $state(0);
-    let cursorY = $state(0);
-    let centerX = $state(0);
-    let centerY = $state(0);
-    // coordinates of the cursor and center
-    function pointermove (event: PointerEvent) {
-        cursorX = (event.clientX - canvas.width / 2 - offsetX) / (scale * gridSize);
-        cursorY = (event.clientY - canvas.height / 2 - offsetY) / (scale * gridSize);
-        centerX = -offsetX / (scale * gridSize);
-        centerY = -offsetY / (scale * gridSize);
-        
-        draw();
-    }
+		// keep zoom on the cursor
+		const cursorX = event.clientX - canvas.width / 2;
+		const cursorY = event.clientY - canvas.height / 2;
 
-    $effect(() => {
-        $coordinates = {
-            x: centerX,
-            y: centerY,
-            z: scale,
-            mx: cursorX,
-            my: cursorY
-        }
-    });
+		offsetX = cursorX - (cursorX - offsetX) * (scale / oldScale);
+		offsetY = cursorY - (cursorY - offsetY) * (scale / oldScale);
+
+		draw();
+	}
+
+	// resize canvas
+	function resize() {
+		canvas.height = window.innerHeight;
+		canvas.width = window.innerWidth;
+		draw();
+	}
+
+	// coordinates of the cursor and center
+	function pointermove(event: PointerEvent) {
+		cursorX = (event.clientX - canvas.width / 2 - offsetX) / (scale * gridSize);
+		cursorY = (event.clientY - canvas.height / 2 - offsetY) / (scale * gridSize);
+		centerX = -offsetX / (scale * gridSize);
+		centerY = -offsetY / (scale * gridSize);
+
+		if (isDragging) {
+			event.preventDefault();
+
+			offsetX = event.clientX - startX;
+			offsetY = event.clientY - startY;
+		}
+
+		draw();
+	}
+
+	$effect(() => {
+		$coordinates = {
+			x: centerX,
+			y: centerY,
+			z: scale,
+			mx: cursorX,
+			my: cursorY
+		};
+	});
 </script>
 
-<canvas class="absolute inset-0" bind:this={canvas}
-    onmousedown={mousedown}
-    onmouseup={mouseup}
-    onmousemove={mousemove}
-    onmouseleave={mouseleave}
-    onwheel={wheel}
-    onresize={resize}
-    onpointermove={pointermove}
+<canvas
+	class="absolute inset-0"
+	style="touch-action: none;"
+	bind:this={canvas}
+	onpointerdown={pointerdown}
+	onpointerup={pointerup}
+	onpointerleave={pointerleave}
+	onwheel={wheel}
+	onpointermove={pointermove}
 ></canvas>
